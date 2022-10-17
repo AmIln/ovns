f35534a616127efaf618e01e07ff8564d328d5b8
Dmitry Andriyanov <facklak@mail.ru>
2022-10-17 18:03:10 +0300

    feat: add new style image-swapper from DC5000

---
+++ js/DC5000.js
@@ -0,0 +1,18 @@
+const imageList = document.querySelector('.images-switch');
+imageList.addEventListener('click', function(e) {
+    let activeImg = document.querySelector('.images-switch_active'),
+    imagesArray = document.querySelector('.images-wrapper').children;
+    position = 0;
+    if (e.target.alt) {
+        activeImg.classList.remove('images-switch_active');
+        e.target.classList.add('images-switch_active');
+        alt = e.target.alt
+        position = Number(alt[alt.length-1]) - 1;
+    }
+    if (position != 0) {
+        let left = '-' + position + '00%';
+        document.querySelector('.images-wrapper').style.left = left;
+    } else {
+        document.querySelector('.images-wrapper').style.left = 0;
+    }
+})
