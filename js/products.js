97c1b54ac071a320f2b77d4ce9196cc269817deb
Dmitry Andriyanov <facklak@mail.ru>
2022-10-17 19:17:50 +0300

    feat: fix image-swapper from FULL products

--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 console.log('products')
 const imageList = document.querySelector('.images-switch');
 /* ПЕРЕКЛЮЧЕНИЕ КАРТИНОК */
-imageList.addEventListener('click', function(e) {
+/*imageList.addEventListener('click', function(e) {
     let activeImg = document.querySelector('.images-switch_active'),
     imagesArray = document.querySelector('.images-wrapper').children;
     position = 0;
@@ -14,6 +14,23 @@ imageList.addEventListener('click', function(e) {
                 position = i;
             }
         }
+    }
+    if (position != 0) {
+        let left = '-' + position + '00%';
+        document.querySelector('.images-wrapper').style.left = left;
+    } else {
+        document.querySelector('.images-wrapper').style.left = 0;
+    }
+})*/
+imageList.addEventListener('click', function(e) {
+    let activeImg = document.querySelector('.images-switch_active'),
+    imagesArray = document.querySelector('.images-wrapper').children;
+    position = 0;
+    if (e.target.alt) {
+        activeImg.classList.remove('images-switch_active');
+        e.target.classList.add('images-switch_active');
+        alt = e.target.alt
+        position = Number(alt[alt.length-1]) - 1;
     }
     if (position != 0) {
         let left = '-' + position + '00%';
