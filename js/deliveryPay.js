fa2b821a82574b5fa4ed46454d18b2a7bbd18672
Dmitry Andriyanov <facklak@mail.ru>
2022-12-14 20:10:20 +0300

    feat: after adaptive

--- js/deliveryPay.js
+++ js/deliveryPay.js
@@ -1,9 +1,7 @@
-console.log('hi')
 let choiseDelivery = document.querySelector('.choise-delivery'),
 choisePay = document.querySelector('.choise-pay'),
 contentDelivery = document.querySelector('.content-delivery'),
 contentPay = document.querySelector('.content-pay');
-
 
 // Доставка
 choiseDelivery.addEventListener('click', function() {
@@ -13,7 +11,6 @@ choiseDelivery.addEventListener('click', function() {
     contentPay.classList.remove('content-text_active');
     contentDelivery.classList.add('content-text_active');
 })
-
 
 // Оплата
 choisePay.addEventListener('click', function() {
