f6addb7ee66a7cedab00a5cc12c30cb0ce7d18ca
Dmitry Andriyanov <facklak@mail.ru>
2022-12-14 13:13:21 +0300

    feat: add choise block from delivery-pay page

---
+++ js/deliveryPay.js
@@ -0,0 +1,25 @@
+console.log('hi')
+let choiseDelivery = document.querySelector('.choise-delivery'),
+choisePay = document.querySelector('.choise-pay'),
+contentDelivery = document.querySelector('.content-delivery'),
+contentPay = document.querySelector('.content-pay');
+
+
+// Доставка
+choiseDelivery.addEventListener('click', function() {
+    choisePay.classList.remove('choise_active');
+    choiseDelivery.classList.add('choise_active');
+
+    contentPay.classList.remove('content-text_active');
+    contentDelivery.classList.add('content-text_active');
+})
+
+
+// Оплата
+choisePay.addEventListener('click', function() {
+    choiseDelivery.classList.remove('choise_active');
+    choisePay.classList.add('choise_active');
+
+    contentDelivery.classList.remove('content-text_active');
+    contentPay.classList.add('content-text_active');
+})
